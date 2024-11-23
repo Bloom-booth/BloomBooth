7755b3607fb5afb454bef6822ee5877c56f4c392
yeonjae02 <0yeonjae2@naver.com>
2024-11-24 02:15:18 +0900

    feat: AuthActivity 기본 구현
    - 파이어베이스에서 정보 받아오기
    - 프레임 레이아웃 틀 구현

---
+++ app/src/main/java/com/example/bloombooth/AuthActivity.kt
@@ -0,0 +1,38 @@
+package com.example.bloombooth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.bloombooth.databinding.ActivityAuthBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class AuthActivity : AppCompatActivity() {
+    val binding: ActivityAuthBinding by lazy { ActivityAuthBinding.inflate(layoutInflater) }
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(binding.root.id, LoginFragment())
+                .commit()
+        }
+    }
+
+    fun navigateToSignUpFragment() {
+        supportFragmentManager.beginTransaction()
+            .replace(binding.root.id, RegisterFragment())
+            .addToBackStack(null)
+            .commit()
+    }
+
+    fun navigateToLoginFragment() {
+        supportFragmentManager.beginTransaction()
+            .replace(binding.root.id, LoginFragment())
+            .addToBackStack(null)
+            .commit()
+    }
+}
