2d3545481d56eceda9bf0c324185f117a749bec1
yeonjae02 <0yeonjae2@naver.com>
2024-11-28 00:11:46 +0900

    chore: 메니페스토 파일 수정 및 실행을 위한 임시 코드 추가
    - 앱 실행 시 스플래시부터 실행되도록 설정
    - auth activity 코드 초기 세팅

---
+++ app/src/main/java/com/example/bloombooth/AuthActivity.kt
@@ -0,0 +1,23 @@
+package com.example.bloombooth
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.bloombooth.databinding.ActivityAuthBinding
+class AuthActivity : AppCompatActivity() {
+
+    val binding: ActivityAuthBinding by lazy { ActivityAuthBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
