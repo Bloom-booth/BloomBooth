ae6366983e061b8c5edfa6b9ebf3e64c31bcbd7e
yeonjae02 <0yeonjae2@naver.com>
2024-12-01 04:01:19 +0900

    feat: 회원 탈퇴 페이지 xml 구현

---
+++ app/src/main/java/com/example/bloombooth/WithdrawalActivity.kt
@@ -0,0 +1,15 @@
+package com.example.bloombooth
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.bloombooth.databinding.ActivityWithdrawalBinding
+
+class WithdrawalActivity : AppCompatActivity() {
+    val binding: ActivityWithdrawalBinding by lazy {ActivityWithdrawalBinding.inflate(layoutInflater)}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+    }
+}
