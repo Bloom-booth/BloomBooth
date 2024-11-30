75ef0728e49af791897f4ab4568aa8de89aa4b7f
yeonjae02 <0yeonjae2@naver.com>
2024-12-01 03:35:29 +0900

    feat: 사용자 개인정보 수정 페이지 xml 구현

---
+++ app/src/main/java/com/example/bloombooth/PersonalInfoActivity.kt
@@ -0,0 +1,17 @@
+package com.example.bloombooth
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.bloombooth.databinding.ActivityPersonalInfoBinding
+
+class PersonalInfoActivity : AppCompatActivity() {
+    val binding: ActivityPersonalInfoBinding by lazy {ActivityPersonalInfoBinding.inflate(layoutInflater)}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        }
+}
