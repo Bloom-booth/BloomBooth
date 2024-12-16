9e53e293d0e8d50679994e1f5dd24af0b71028ad
yeonjae02 <0yeonjae2@naver.com>
2024-12-17 02:53:43 +0900

    feat: 리뷰 이미지 업로드를 위한 요소 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation(libs.androidx.runner)
     implementation(libs.androidx.espresso.intents)
     testImplementation(libs.junit)
+    implementation("com.github.bumptech.glide:glide:4.15.1")
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation("androidx.test:runner:1.5.2")
@@ -67,6 +68,8 @@ dependencies {
     androidTestImplementation(libs.androidx.test.core)
     androidTestImplementation(libs.fragment.testing)
     androidTestImplementation(libs.mockito)
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
 
     implementation(platform("com.google.firebase:firebase-bom:33.6.0"))
     // TODO: Add the dependencies for Firebase products you want to use
