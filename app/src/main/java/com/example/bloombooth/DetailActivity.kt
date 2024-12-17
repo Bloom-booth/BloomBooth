b5faa1eb3600dd975f699cb6557de775c386c9bd
yeonjae02 <0yeonjae2@naver.com>
2024-12-17 23:10:21 +0900

    wip: 최소한의 연결 구현

--- app/src/main/java/com/example/bloombooth/DetailActivity.kt
+++ app/src/main/java/com/example/bloombooth/DetailActivity.kt
@@ -1,20 +1,22 @@
 package com.example.bloombooth
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.bloombooth.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
+    val binding : ActivityDetailBinding by lazy {ActivityDetailBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_detail)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        binding.writeReviewBtn.setOnClickListener {
+            val intent = Intent(this, ReviewActivity::class.java)
+            intent.putExtra("boothName", "현재 가게 이름")  // value에 가게 이름 넣어주세요!
+            startActivity(intent)
         }
     }
 }
